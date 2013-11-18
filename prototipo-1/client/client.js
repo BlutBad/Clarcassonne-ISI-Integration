bca1ad4677709bf4317e4af40dd74b927c7186f2
Adrian Pizarro Serrano <adrianpizarroserrano@hotmail.com>
2013-11-18 18:24:59 +0100

    detectado cuenta creada

--- prototipo-1/client/client.js
+++ prototipo-1/client/client.js
@@ -1,22 +1,37 @@
-//user = "adri";
-//password = "123456";
-//$("#login-buttons-password").click(function(){
-//	alert("bienvenido");
-//});
+
+var usersLoaded = false;
 
 $(document).ready(function() {
 		$('#coin-slider').coinslider({ width: 500, height:500 });
 	});
+ 
 
+Meteor.subscribe("users", function () {
 
-Template.loginButtons.events({
-	'click': function () {
-		alert("as");
+	usersLoaded = true;
+
+});
+
+Meteor.users.find().observe({
+ 
+	changed: function(user) {
+
+		if (usersLoaded) {
+
+			console.log("New user created: ", user);
+			Meteor.Collection("users").insert({name: names[i], score: randomNum()});
+		}
+
 	}
+	
+	changed
 });
 
 Accounts.ui.config({
-
+	
 	passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
 
 });
+
+
+
