f798ee5f7fb5081527bedadb6a4284c1c03fa279
Serhii Slipenko <LeSagra@gmail.com>
2013-11-20 20:40:22 +0100

    Creat seccion

--- client/js/juegos.js
+++ client/js/juegos.js
@@ -1,3 +1,27 @@
 Template.juegos.show = function() {
   return Session.get('current_stage') == 'Juegos';
 };
+
+
+Template.juegos.juegos = function() {
+  
+  return Juegos.find({});
+}
+
+
+Template.juegos.events({
+
+  'click img' : function() {
+    console.log(this.name);
+     //Si estas pinchando sobre el mismo tag que ya esta seleccionado
+    if (Session.equals('current_stage', this.name)){
+    // Poner lo a null, es decir celeccionar por defecto
+      Session.set('current_stage', 'Dashboard');
+    }else{
+    // De otra manera apuntar nuevo id del tag seleccionado
+      Session.set('current_stage', this.name);
+    }
+    
+}
+    
+});
