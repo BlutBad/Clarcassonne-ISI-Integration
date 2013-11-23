cd70b0958542a5d3deba3d2ecee72a36188e8fa3
itziar <itziar276@gmail.com>
2013-11-23 10:31:36 +0100

    boton crear torneo y div para incluir el torneo

--- client/js/torneos.js
+++ client/js/torneos.js
@@ -1,8 +1,34 @@
 
-
-
-
-
 Template.torneos.show = function() {
   return Session.get('current_stage') == 'Torneos';
+};
+
+Template.torneos.events = {
+  'click input#crear_torneo': function() {
+   		openCreateDialog();
+   }
+};
+
+var openCreateDialog = function () {
+  Session.set("createError", null);
+  Session.set("showCreateDialog", true);
+};
+
+Template.torneos.showCreateDialog = function () {
+  return Session.get("showCreateDialog");
+};
+
+Template.createDialog.events({
+  'click .save': function (event, template) {
+  		return Session.get('current_stage') == 'showCreateDialog';
+    
+  },
+
+  'click .cancel': function () {
+    Session.set("showCreateDialog", false);
+  }
+});
+
+Template.createDialog.error = function () {
+  return Session.get("createError");
 };
