dcf86f690d02039e5498ae545dac31252b200064
yanaisa13 <yanaisagonzalez@gmail.com>
2013-11-26 11:18:08 +0100

    Creo prototipo 3.

---
+++ prototipo-3/server/server.js
@@ -0,0 +1,18 @@
+Meteor.publish("users", function() {
+ 	
+	return Meteor.users.find();
+
+});
+
+Meteor.publish("messages", function() {
+ 	
+	return Messages.find();
+
+});
+
+Meteor.publish("games", function() {
+ 	
+	return Games.find();
+
+});
+
