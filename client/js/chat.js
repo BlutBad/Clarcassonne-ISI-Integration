65d79e08448a0bf799093093a4c278f7d757cbc0
vdjimenez <vd.jimenez89@gmail.com>
2013-11-27 16:39:55 +0100

    v2

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,21 +1,72 @@
 // --- Begin: Chat Global ---
 
-Template.gblmsgs.msgs = function() {
-  return Global_msgs.find({},{sort: {time: -1}});
-};
+// Template.gblmsgs.msgs = function() {
+//   return Global_msgs.find({},{sort: {time: -1}});
+// };
 
-Template.wrmsg.events = {
-  'click input#sendmsg': function() {
-    if (Meteor.user()) {
-      var user = Meteor.user().emails[0].address;
-    } else {
-      var user = 'Unknown';
-    }
-    var msg = $('input#msg');
-    if (msg.val() != '') {
-        Global_msgs.insert({user:user, msg:msg.val(), time:Date.now});
-        msg.val('');
-      }
-    }
-};
+Template.chat = function() {
+  return Session.get('var1') == 'ok';
+}
+
+// Template.wrmsg.events = {
+//   'click input#sendmsg': function() {
+//     if (Meteor.user()) {
+//       var user = Meteor.user().emails[0].address;
+//     } else {
+//       var user = 'Unknown';
+//     }
+//     var msg = $('input#msg');
+//     if (msg.val() != '') {
+//         Global_msgs.insert({user:user, msg:msg.val(), time:Date.now});
+//         msg.val('');
+//       }
+//     }
+// };
+
+// --------- Prueba 1
+// $(document).ready(function(){
+//   $("#gblchat").chatbox({
+//     id: "danny",
+//     user: {key: "value"},
+//     title: "test",
+//     messageSent: function(id, user, msg){
+//       $("#gblchat").chatbox("option", "boxManager").addMsg(id, msg);
+//     }
+//   });
+// });
+
+
+// -------- Prueba 2
+
+// chat = {
+//   setup: function() {
+//     $("#gblchat").chatbox({
+//       id: "danny",
+//       user: {key: "value"},
+//       title: "test",
+//       messageSent: function(id, user, msg){
+//         $("#gblchat").chatbox("option", "boxManager").addMsg(id, msg);
+//       }
+//     })
+//   }
+// }
+
+
+// $(chat.setup);
+
+
+ // $(document).ready(function(){
+ //          // to create
+ //          $("#chat_div").chatbox({id : "chat_div",
+ //                                  title : "Title",
+ //                                  user : "can be anything",
+ //                                  offset: 200,
+ //                                  messageSent: function(id, user, msg){
+ //                                       alert("DOM " + id + " just typed in " + msg);
+ //                                  }});
+ //          // to insert a message
+ //          $("#chat_div").chatbox("option", "boxManager").addMsg("Mr. Foo", "Barrr!");
+ //      });
+
+//});
 // --- End: Chat Global ---
