3ea549c1ea9d149c4af990a92de14fff02df5db4
saravila <s.vilarinol@gmail.com>
2013-11-27 19:01:56 +0100

    Añadida sección para filtrar ranking por juegos en el div de ranking

--- client/js/ranking.js
+++ client/js/ranking.js
@@ -2,11 +2,10 @@ Template.ranking.show = function() {
 	return Session.get('current_stage') == 'Ranking';
 };
 
-
-
-
-
-
 Template.ranking.scores = function() {
 	return Ranking.find({});
 }
+
+Template.ranking.juegos=function(){
+	return Juegos.find({});
+};
