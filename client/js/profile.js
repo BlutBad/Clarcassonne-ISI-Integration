152f61e39d6bd5e6926ad0bd080a3311f8329a99
itziar <itziar276@gmail.com>
2013-12-03 14:16:09 +0100

    arreglado que el perfil de usuario solo salga cuando estas logeado

--- client/js/profile.js
+++ client/js/profile.js
@@ -6,13 +6,24 @@ Template.profil.showprofile = function() {
 	return Session.get('current_stage') == 'Mi perfil';
 };
 
+Template.profil.registrado=function(){
+	if (Meteor.user()) {
+		return true;
+	}else{
+		return false;
+	}
+
+};
+
 Template.profil.events({
 	'click #datebirthday': function(){
 		$(function() {
     		$("#datebirthday").datepicker({
     		  	showOn: "button",
 				buttonImage: "images/calendar.gif",
-				buttonImageOnly: true,
+				buttonImageOnly: false,
+				changeMonth: true,
+      			changeYear: true,
 				dateFormat: "yy-mm-dd"
 			});
 		});
