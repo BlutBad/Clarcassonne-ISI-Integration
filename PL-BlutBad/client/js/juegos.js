d7bd14ea813ace71f16f84cd29a15f72f206b69a
Serhii Slipenko <LeSagra@gmail.com>
2013-12-17 15:31:31 +0100

    Definir un poco mejor la logica para juegos mono-multi jugador,

--- PL-BlutBad/client/js/juegos.js
+++ PL-BlutBad/client/js/juegos.js
@@ -6,73 +6,98 @@ Template.juegos.juegos = function() {
     return Juegos.find({});
 };
 
-Template.juegos.editar=function(){
-	if (Meteor.user()){
-		if (Meteor.user().username=="admin") {
-    		return true;
- 		}else{
- 			return false;
- 		};
- 	};
+Template.juegos.editar = function() {
+    if (Meteor.user()) {
+	if (Meteor.user().username == "admin") {
+	    return true;
+	} else {
+	    return false;
+	}
+	;
+    }
+    ;
 };
 
 Template.juegos.events({
     'click img' : function() {
-		// Si estas pinchando sobre el mismo tag que ya esta seleccionado
-		if (Session.equals('current_stage', this.name)) {
-		    Session.set('current_stage', 'Juegos');
-		    Session.set('load_game', null);
-		} else {
-			if (this.name == 'Clarcassone') {
-		    	Session.set('current_stage', "klarkiHall");
-			} else {
-		    	Session.set('current_stage', this.name);
-		    	Session.set('load_game', this);
-			}
-
-		}
+	// Si estas pinchando sobre el mismo tag que ya esta seleccionado
+	if (Session.equals('current_stage', this.name)) {
+	    Session.set('current_stage', 'Juegos');
+	    Session.set('load_game', null);
+	} else {
+	    Session.set('current_stage', false);
+	    Session.set('load_game', this);
+	}
     },
     'click .edit_game' : function() {
-		Session.set('gameToEdit', this._id);
-		//console.log(this._id);
+	Session.set('gameToEdit', this._id);
+	// console.log(this._id);
     }
-}); 
+});
 
+/*
+ * if (Meteor.user()) { if (Meteor.user().username == "admin") { return true; }
+ * else { return false; }; }; };
+ * 
+ */
+
+// Los juegos se dividen en dos grupos,
+// - Solo, para solo un jugador
+// - Multi para mas de un jugador, hay que crear partidas, por eso hay que pasar
+// a un estado
+// - de creacion de partidas y luego ya arrancar la partida.
 Deps.autorun(function(c) {
-    lg = Session.get('load_game'); 
-    if (lg && lg.mode == "solo") {  
-		$('#gamecontainer').show();
-		Session.set('showGameIdn', lg.idn);
-		Session.set("current_game", lg._id);
-		eval(lg.wrapf); 
-    } else { 
-		$('#gamecontainer').hide();
-		Session.set('showGameIdn', null); 
+    // lg - el juego sobre el cual se acaba de pinchar.
+    lg = Session.get('load_game');
+    if (lg) {
+	Session.set("current_game", lg._id);
+	// Si es solo, mostramos el game container y el canvas del juego, y
+	// seguidamente lo lanzamos.
+	if (lg.mode == "solo") {
+	    $('#gamecontainer').show();
+	    Session.set('showGameIdn', lg.idn);
+	    // current_game - es necesaria para el juego, con el id del juego luego
+	    // se apunta al jugador en el ranking.
+	    Session.set("current_game", lg._id);
+	    // Se lanza el juego.
+	    eval(lg.wrapf);
+	} else if (lg.mode == "multi") {
+	    // Si el modo el multi, vamos a pasarl al estado del HALL del juego,
+	    // Asi podemos tener muchos juego multi, cada uno con su HALL, y
+	    // cargarlo el hall si mucho sufrimiento.
+	    Session.set('current_stage', lg.hall)
+	}
+    } else {
+	// Se esconde el contenedor de juegos.
+	$('#gamecontainer').hide();
+	Session.set('showGameIdn', false);
     }
 });
 
 Deps.autorun(function(c) {
     if (Session.equals('showGameIdn', 'froot')) {
-		$('#gameFrootcontainer').show();
+	$('#gameFrootcontainer').show();
     } else {
-		$('#gameFrootcontainer').hide();
+	$('#gameFrootcontainer').hide();
     }
 });
 
 Deps.autorun(function(c) {
     if (Session.equals('showGameIdn', 'alien')) {
-		$('#gamecanvasAlien').show();
+	$('#gamecanvasAlien').show();
     } else {
-		$('#gamecanvasAlien').hide();
+	$('#gamecanvasAlien').hide();
     }
 });
 
 Deps.autorun(function(c) {
-	console.log(Session.get("showGameIdn"))
+    console.log(Session.get("showGameIdn"))
     if (Session.equals('showGameIdn', 'clarki')) {
-		$('#CanvasclarcaGame').show();
+	// Para los multijuegos, mostramos el contenedor junto con el juego.
+	$('#gamecontainer').show();
+	$('#CanvasclarcaGame').show();
     } else {
-		$('#CanvasclarcaGame').hide();
+	$('#CanvasclarcaGame').hide();
     }
 });
 
@@ -80,52 +105,53 @@ Template.gamecontainer.render = function() {
     $('#gamecontainer').show();
 };
 
-///////////////////////////EDIT GAME DESCRIPTION//////////////////////////////
+// /////////////////////////EDIT GAME DESCRIPTION//////////////////////////////
 
 Template.juegos.showEditGame = function() {
-	if (Meteor.user()){
-		if (Meteor.user().username=="admin") {
-    		return !Session.equals('gameToEdit', null);
- 		};
- 	};
+    if (Meteor.user()) {
+	if (Meteor.user().username == "admin") {
+	    return !Session.equals('gameToEdit', null);
+	}
+	;
+    }
+    ;
 }
-
 
 Template.editGame.game = function() {
     var id = Session.get('gameToEdit');
-    //console.log(id);
+    // console.log(id);
     return Juegos.findOne({
-		_id : id
+	_id : id
     });
 };
 
 Template.editGame.events({
 
     'click .save' : function(event, template) {
-		var gid = Session.get('gameToEdit');
+	var gid = Session.get('gameToEdit');
 
-		var title_desc = template.find("#title_desc").value;
-		var logo_src = template.find("#logo_src").value;
-		var name = template.find("#name").value;
-		var description = template.find("#description").value;
-		var wrapf = template.find("#wrapf").value;
-		//console.log(name + ' ' + description);
+	var title_desc = template.find("#title_desc").value;
+	var logo_src = template.find("#logo_src").value;
+	var name = template.find("#name").value;
+	var description = template.find("#description").value;
+	var wrapf = template.find("#wrapf").value;
+	// console.log(name + ' ' + description);
 
-		Juegos.update(gid, {
-		    $set : {
-				name : name,
-				logo_src : logo_src,
-				title_desc : title_desc,
-				description : description,
-				wrapf : wrapf,
-		    }
-		});
+	Juegos.update(gid, {
+	    $set : {
+		name : name,
+		logo_src : logo_src,
+		title_desc : title_desc,
+		description : description,
+		wrapf : wrapf,
+	    }
+	});
 
-		Session.set('gameToEdit', null);
+	Session.set('gameToEdit', null);
     },
 
     'click .cancel' : function() {
-		Session.set('gameToEdit', null); 
+	Session.set('gameToEdit', null);
     }
 
 });
