7bf92c87875aac37339fee0d657c6c376de01342
Serhii Slipenko <LeSagra@gmail.com>
2013-12-18 01:23:40 +0100

    Commit Gratis

--- PL-BlutBad/client/js/hall-clarcassone.js
+++ PL-BlutBad/client/js/hall-clarcassone.js
@@ -53,6 +53,8 @@ notRegister = function() {
     });
 }
 
+
+
 Template.hall_clarcassone
 	.events({
 	    'click #nuevaPartida' : function() {
@@ -111,21 +113,22 @@ Template.hall_clarcassone
 		    
 		    usersJoined = PartidasVolatiles.findOne({
 			_id : this._id
-		    }).jugadores;
+		    });
 		    
 		    
 		    
-		    if (usersJoined.length < 1 || usersJoined.length > 5) {
+		    if (usersJoined.jugadores.length < 1 || usersJoined.jugadores.length > 5) {
 			Session.set("createError","Para empezar una partida deben unirse de 3 a 5 jugadores");
 		    } else {
 			var party_jugadores = [];
 			
 			for (var i=0, l=usersJoined.length; i<l;i++){
-			    party_jugadores.push({user_id:usersJoined[i].user_id});
+			    party_jugadores.push({user_id:usersJoined.jugadores[i].user_id});
 			}
 
 			//id de la partida que ha sido creada.
 			party_id = Partidas.insert({
+			    _id :usersJoined._id,
 			    jugadores : party_jugadores,
 			    terminada : false,
 			});
@@ -135,6 +138,8 @@ Template.hall_clarcassone
 			//eval("ClarcassonneGameIU.initialize(idCanvasElement, party_id)");
 			
 			//Id del canvas donde se va a pintar el juego
+			
+			
 			
 			setTimeout(function() {
 			    ClarcassonneGameIU.initialize('#CanvasclarcaGame', party_id);
