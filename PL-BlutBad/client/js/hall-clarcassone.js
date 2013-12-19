0f1ffcfcc851f473d2dfbd6b6b1c8c3c4a787934
saravila <s.vilarinol@gmail.com>
2013-12-19 14:21:58 +0100

    Ya comparten id de partida los usuarios apuntados a una partidavolatil por lo que se muestra el canvas del juego clarca en los 3 clientes cuando el creador pulsa el botón comenzar

--- PL-BlutBad/client/js/hall-clarcassone.js
+++ PL-BlutBad/client/js/hall-clarcassone.js
@@ -53,195 +53,228 @@ notRegister = function() {
     });
 }
 
-Template.hall_clarcassone
-        .events({
-            'click #nuevaPartida' : function() {
-                if (Meteor.userId()) {
-                    userA = UsersInHall.findOne({
-                        user_id : Meteor.userId()
+Template.hall_clarcassone.events({
+    'click #nuevaPartida' : function() {
+        if (Meteor.userId()) {
+            userA = UsersInHall.findOne({
+                user_id : Meteor.userId()
+            });
+            userCreator = PartidasVolatiles.findOne({
+                creator_id : Meteor.userId()
+            })
+            if (userCreator) {
+                Session.set("createError", "Ya creaste una partida!");
+            } else {
+                if (userA) {
+                    // ^.^ edad = Math.floor(Random.fraction() * 70);
+                    PartidasVolatiles.insert({
+                        creator_id : Meteor.userId(),
+                        jugadores : [ {
+                            user_id : Meteor.userId(),
+                            estado : estadosU.pendiente,
+                        } ],
+                        listos: false
                     });
-                    userCreator = PartidasVolatiles.findOne({
-                        creator_id : Meteor.userId()
-                    })
-                    if (userCreator) {
-                        Session.set("createError", "Ya creaste una partida!");
-                    } else {
-                        if (userA) {
-                            // ^.^ edad = Math.floor(Random.fraction() * 70);
-                            PartidasVolatiles.insert({
-                                creator_id : Meteor.userId(),
-                                jugadores : [ {
-                                    user_id : Meteor.userId(),
-                                    estado : estadosU.pendiente,
-                                } ]
-                            });
-                            UsersInHall.remove(userA._id);
-                            Session.set("createError", undefined);
-                        } else {
-                            Session
-                                    .set("createError",
-                                            "Ya estás en una partida.... no crees otra! :D");
-                        }
-                    }
+                    UsersInHall.remove(userA._id);
+                    Session.set("createError", undefined);
                 } else {
-                    Session.set("createError",
-                            "Regístrate para crear partidas!");
-                }
-            },
-            'click .removeparty' : function() {
-                if (Meteor.userId()) {
-                    if (Meteor.userId()) {
-                        userCreator = PartidasVolatiles.findOne({
-                            creator_id : Meteor.userId()
-                        });
-                        if (userCreator) {
-                            PartidasVolatiles.remove(this._id);
-                            Session.set("createError", undefined);
-                        } else {
-                            Session
-                                    .set("createError",
-                                            "No borres partidas que no has creado tú.... o.o");
-                        }
-                    }
-                } else {
-                    notRegister();
-                }
-            },
-            'click .startparty' : function() {
-
-                if (Meteor.userId()) {
-
-                    usersJoined = PartidasVolatiles.findOne({
-                        _id : this._id
-                    }).jugadores;
-
-                    if (usersJoined.length < 1 || usersJoined.length > 5) {
-                        Session
-                                .set("createError",
-                                        "Para empezar una partida deben unirse de 3 a 5 jugadores");
-                    } else {
-                        var party_jugadores = [];
-
-                        for ( var i = 0, l = usersJoined.length; i < l; i++) {
-                            party_jugadores.push({
-                                user_id : usersJoined[i].user_id
-                            });
-                        }
-
-                        // id de la partida que ha sido creada.
-                        party_id = Partidas.insert({
-                            jugadores : party_jugadores,
-                            terminada : false,
-                        });
-
-                        console
-                                .log('eval("ClarcassonneGameIU.initialize(idCanvasElement, party_id)");');
-                        // eval("ClarcassonneGameIU.initialize(idCanvasElement,
-                        // party_id)");
-
-                        // Id del canvas donde se va a pintar el juego
-
-                        setTimeout(function() {
-                            ClarcassonneGameIU.initialize('#CanvasclarcaGame',
-                                    party_id);
-                        }, 500);
-
-                        // Para que se muestre el canvas del juego,
-                        Session.set('showGameIdn', "clarki");
-
-                        // Para esconder el hall, solo se ve el canvas
-                        Session.set('current_stage', false);
-                    }
-                    // Hacer una entrada a la coleccion de Partidas,
-                    // y llamar a ui y ai con ese _id de la partida.
-                } else {
-                    notRegister();
-                }
-            },
-
-            'click .unirme' : function() {
-                // console.log('Unirme a una partida');
-
-                if (Meteor.userId()) {
-
-                    usersJoined = PartidasVolatiles.findOne({
-                        _id : this._id
-                    }).jugadores;
-                    usersinParty = false;
-                    usersJoined.forEach(function(each) {
-                        if (each.user_id == Meteor.userId()) {
-                            usersinParty = true;
-                        }
-                    });
-                    // console.log(usersinParty)
-                    userA = UsersInHall.findOne({
-                        user_id : Meteor.userId()
-                    });
-                    if (!usersinParty) {
-                        PartidasVolatiles.update(this._id, {
-                            $push : {
-                                jugadores : {
-                                    user_id : Meteor.userId(),
-                                    estado : estadosU.pendiente,
-                                }
-                            }
-                        });
-                        if (userA) {
-                            UsersInHall.remove(userA._id);
-                        }
-                        Session.set("createError", undefined);
-                    } else {
-                        userCreator = PartidasVolatiles.findOne({
-                            _id : this._id
-                        }).creator_id;
-                        if (userCreator == Meteor.userId()) {
-                            Session
-                                    .set("createError",
-                                            "Si quieres abandonar debes descartar la partida! Eres el creador...");
-                        } else {
-                            usersJoined = _.without(usersJoined, _.findWhere(
-                                    usersJoined, {
-                                        user_id : Meteor.userId()
-                                    }));
-                            PartidasVolatiles.update(this._id, {
-                                creator_id : userCreator,
-                                jugadores : usersJoined
-                            });
-                            Session.set("createError", undefined);
-                        }
-                    }
-                } else {
-                    notRegister();
-                }
-            },
-            'click .ready' : function() {
-
-                if (Meteor.userId()) {
-                    usersJoined = PartidasVolatiles.findOne({
-                        _id : this._id
-                    });
-                    userCreator = usersJoined.creator_id;
-                    usersJoined = usersJoined.jugadores;
-                    usersJoined.forEach(function(each) {
-                        if (each.user_id == Meteor.userId()) {
-                            if (each.estado == estadosU.listo) {
-                                each.estado = estadosU.pendiente;
-                            } else if (each.estado == estadosU.pendiente) {
-                                each.estado = estadosU.listo;
-                            } else {
-                                each.estado = estadosU.inactivo;
-                            }
-                        }
-                    });
-                    PartidasVolatiles.update(this._id, {
-                        creator_id : userCreator,
-                        jugadores : usersJoined
-                    });
-                } else {
-                    notRegister();
+                    Session
+                            .set("createError",
+                                    "Ya estás en una partida.... no crees otra! :D");
                 }
             }
-        });
+        } else {
+            Session.set("createError",
+                    "Regístrate para crear partidas!");
+        }
+    },
+    'click .removeparty' : function() {
+        if (Meteor.userId()) {
+            if (Meteor.userId()) {
+                userCreator = PartidasVolatiles.findOne({
+                    creator_id : Meteor.userId()
+                });
+                if (userCreator) {
+                    PartidasVolatiles.remove(this._id);
+                    Session.set("createError", undefined);
+                } else {
+                    Session
+                            .set("createError",
+                                    "No borres partidas que no has creado tú.... o.o");
+                }
+            }
+        } else {
+            notRegister();
+        }
+    },
+    'click .startparty' : function() {
+
+        if (Meteor.userId()) {
+
+            usersJoined = PartidasVolatiles.findOne({
+                _id : this._id
+            }).jugadores;
+            userCreator = PartidasVolatiles.findOne({
+                creator_id : Meteor.userId()
+            })
+
+            if (usersJoined.length < 1 || usersJoined.length > 5) {
+                Session
+                        .set("createError",
+                                "Para empezar una partida deben unirse de 3 a 5 jugadores");
+            } else {
+                if (userCreator){
+                    var party_jugadores = [];
+
+                    for ( var i = 0, l = usersJoined.length; i < l; i++) {
+                        party_jugadores.push({
+                            user_id : usersJoined[i].user_id
+                        });
+                    }
+
+                    // id de la partida que ha sido creada.
+                    party_id = Partidas.insert({
+                        jugadores : party_jugadores,
+                        terminada : false,
+                    });
+                    PartidasVolatiles.update(this._id, {
+                        creator_id : Meteor.userId(),
+                        jugadores : usersJoined,
+                        listos: true,
+                        partyid: party_id
+                    });
+
+                    //console.log('eval("ClarcassonneGameIU.initialize(idCanvasElement, party_id)");');
+                    // eval("ClarcassonneGameIU.initialize(idCanvasElement, party_id)");
+
+                    // Id del canvas donde se va a pintar el juego
+
+                    ClarcassonneGameIU.initialize('#CanvasclarcaGame', party_id);
+
+                    // Para que se muestre el canvas del juego,
+                    Session.set('showGameIdn', "clarki");
+
+                    // Para esconder el hall, solo se ve el canvas
+                    Session.set('current_stage', false);
+                }
+            }
+            // Hacer una entrada a la coleccion de Partidas,
+            // y llamar a ui y ai con ese _id de la partida.
+        } else {
+            notRegister();
+        }
+    },
+
+    'click .unirme' : function() {
+        // console.log('Unirme a una partida');
+
+        if (Meteor.userId()) {
+
+            usersJoined = PartidasVolatiles.findOne({
+                _id : this._id
+            }).jugadores;
+            usersinParty = false;
+            usersJoined.forEach(function(each) {
+                if (each.user_id == Meteor.userId()) {
+                    usersinParty = true;
+                }
+            });
+            // console.log(usersinParty)
+            userA = UsersInHall.findOne({
+                user_id : Meteor.userId()
+            });
+            if (!usersinParty) {
+                PartidasVolatiles.update(this._id, {
+                    $push : {
+                        jugadores : {
+                            user_id : Meteor.userId(),
+                            estado : estadosU.pendiente,
+                        }
+                    }
+                });
+                if (userA) {
+                    UsersInHall.remove(userA._id);
+                }
+                Session.set("createError", undefined);
+            } else {
+                userCreator = PartidasVolatiles.findOne({
+                    _id : this._id
+                }).creator_id;
+                if (userCreator == Meteor.userId()) {
+                    Session
+                            .set("createError",
+                                    "Si quieres abandonar debes descartar la partida! Eres el creador...");
+                } else {
+                    usersJoined = _.without(usersJoined, _.findWhere(
+                            usersJoined, {
+                                user_id : Meteor.userId()
+                            }));
+                    PartidasVolatiles.update(this._id, {
+                        creator_id : userCreator,
+                        jugadores : usersJoined,
+                        listos: false
+                    });
+                    Session.set("createError", undefined);
+                }
+            }
+        } else {
+            notRegister();
+        }
+    },
+    'click .ready' : function() {  
+        if (Meteor.userId()) {
+            usersJoined = PartidasVolatiles.findOne({
+                _id : this._id
+            });
+            userCreator = usersJoined.creator_id;
+            usersJoined = usersJoined.jugadores;
+            usersJoined.forEach(function(each) {
+                if (each.user_id == Meteor.userId()) {
+                    if (each.estado == estadosU.listo) {
+                        each.estado = estadosU.pendiente;
+                    } else if (each.estado == estadosU.pendiente) {
+                        each.estado = estadosU.listo;
+                    } else {
+                        each.estado = estadosU.inactivo;
+                    }
+                }
+            });
+            PartidasVolatiles.update(this._id, {
+                creator_id : userCreator,
+                jugadores : usersJoined
+            });
+        } else {
+            notRegister();
+        }
+    }
+});
+
+Deps.autorun(function(c) { 
+    user =  Meteor.userId();
+    // Debería funcionar....
+    /*partidav = PartidasVolatiles.find({
+        jugadores: {$elemMatch: {user_id: user}}
+    }); */ 
+    ready = false;
+    PartidasVolatiles.find({}).forEach(function(each){
+        each.jugadores.forEach(function(each2){
+            if (each2.user_id == user){
+                ready = each.listos;
+                partidav = each;
+            }
+        })
+    });
+    if (ready) {
+        party_id = partidav.partyid; 
+        ClarcassonneGameIU.initialize('#CanvasclarcaGame', party_id); 
+
+        // Para que se muestre el canvas del juego,
+        Session.set('showGameIdn', "clarki");
+
+        // Para esconder el hall, solo se ve el canvas
+        Session.set('current_stage', false);         
+    }
+})
 
 Template.hall_clarcassone.partidasVolatiles = function() {
     return PartidasVolatiles.find({});
--- PL-BlutBad/client/js/juegos.js
+++ PL-BlutBad/client/js/juegos.js
@@ -49,57 +49,57 @@ Deps.autorun(function(c) {
     // lg - el juego sobre el cual se acaba de pinchar.
     lg = Session.get('load_game');
     if (lg) {
-	Session.set("current_game", lg._id);
-	// Si es solo, mostramos el game container y el canvas del juego, y
-	// seguidamente lo lanzamos.
-	if (lg.mode == "solo") {
-	    $('#gamecontainer').show();
-	    Session.set('showGameIdn', lg.idn);
-	    // current_game - es necesaria para el juego, con el id del juego luego
-	    // se apunta al jugador en el ranking.
-	    Session.set("current_game", lg._id);
-	    // Se lanza el juego.
-	    eval(lg.wrapf);
-	} else if (lg.mode == "multi") {
-	    // Si el modo el multi, vamos a pasarl al estado del HALL del juego,
-	    // Asi podemos tener muchos juego multi, cada uno con su HALL, y
-	    // cargarlo el hall si mucho sufrimiento.
-	    Session.set('current_stage', lg.hall)
-	}
-    } else {
-	// Se esconde el contenedor de juegos.
-	$('#gamecontainer').hide();
-	Session.set('showGameIdn', false);
+		Session.set("current_game", lg._id);
+		// Si es solo, mostramos el game container y el canvas del juego, y
+		// seguidamente lo lanzamos.
+		if (lg.mode == "solo") {
+		    $('#gamecontainer').show();
+		    Session.set('showGameIdn', lg.idn);
+		    // current_game - es necesaria para el juego, con el id del juego luego
+		    // se apunta al jugador en el ranking.
+		    Session.set("current_game", lg._id);
+		    // Se lanza el juego.
+		    eval(lg.wrapf);
+		} else if (lg.mode == "multi") {
+		    // Si el modo el multi, vamos a pasarl al estado del HALL del juego,
+		    // Asi podemos tener muchos juego multi, cada uno con su HALL, y
+		    // cargarlo el hall si mucho sufrimiento.
+		    Session.set('current_stage', lg.hall)
+		}
+	} else {
+		// Se esconde el contenedor de juegos.
+		$('#gamecontainer').hide();
+		Session.set('showGameIdn', false);
     }
 });
 
-Deps.autorun(function(c) {
+Deps.autorun(function(c) { 
     if (Session.equals('showGameIdn', 'froot')) {
-	$('#gameFrootcontainer').show();
+		$('#gameFrootcontainer').show();
     } else {
-	$('#gameFrootcontainer').hide();
+		$('#gameFrootcontainer').hide();
     }
 });
 
 Deps.autorun(function(c) {
     if (Session.equals('showGameIdn', 'alien')) {
-	$('#gamecanvasAlien').show();
+		$('#gamecanvasAlien').show();
     } else {
-	$('#gamecanvasAlien').hide();
+		$('#gamecanvasAlien').hide();
     }
 });
 
 Deps.autorun(function(c) {
-    console.log(Session.get("showGameIdn"))
+    //console.log(Session.get("showGameIdn"))
     if (Session.equals('showGameIdn', 'clarki')) {
-	// Para los multijuegos, mostramos el contenedor junto con el juego.
-	$('#gamecontainer').show();
-	$('#CanvasclarcaGame').show();
-	//$('#ClarcaGame').show();
-	
+		// Para los multijuegos, mostramos el contenedor junto con el juego.
+		$('#gamecontainer').show();
+		$('#CanvasclarcaGame').show();
+		//$('#ClarcaGame').show();
+		
     } else {
-	$('#CanvasclarcaGame').hide();
-	//$('#ClarcaGame').hide();
+		$('#CanvasclarcaGame').hide();
+		//$('#ClarcaGame').hide();
     }
 });
 
@@ -111,49 +111,46 @@ Template.gamecontainer.render = function() {
 
 Template.juegos.showEditGame = function() {
     if (Meteor.user()) {
-	if (Meteor.user().username == "admin") {
-	    return !Session.equals('gameToEdit', null);
-	}
-	;
-    }
-    ;
+		if (Meteor.user().username == "admin") {
+		    return !Session.equals('gameToEdit', null);
+		};
+	};
 }
 
 Template.editGame.game = function() {
     var id = Session.get('gameToEdit');
-    // console.log(id);
-    return Juegos.findOne({
-	_id : id
+	    // console.log(id);
+	    return Juegos.findOne({
+		_id : id
     });
 };
 
 Template.editGame.events({
 
     'click .save' : function(event, template) {
-	var gid = Session.get('gameToEdit');
+		var gid = Session.get('gameToEdit');
 
-	var title_desc = template.find("#title_desc").value;
-	var logo_src = template.find("#logo_src").value;
-	var name = template.find("#name").value;
-	var description = template.find("#description").value;
-	var wrapf = template.find("#wrapf").value;
-	// console.log(name + ' ' + description);
+		var title_desc = template.find("#title_desc").value;
+		var logo_src = template.find("#logo_src").value;
+		var name = template.find("#name").value;
+		var description = template.find("#description").value;
+		var wrapf = template.find("#wrapf").value;
+		// console.log(name + ' ' + description);
 
-	Juegos.update(gid, {
-	    $set : {
-		name : name,
-		logo_src : logo_src,
-		title_desc : title_desc,
-		description : description,
-		wrapf : wrapf,
-	    }
-	});
+		Juegos.update(gid, {
+		    $set : {
+			name : name,
+			logo_src : logo_src,
+			title_desc : title_desc,
+			description : description,
+			wrapf : wrapf,
+		    }
+		});
 
-	Session.set('gameToEdit', null);
-    },
-
+		Session.set('gameToEdit', null);
+	},
     'click .cancel' : function() {
-	Session.set('gameToEdit', null);
+		Session.set('gameToEdit', null);
     }
 
 });
