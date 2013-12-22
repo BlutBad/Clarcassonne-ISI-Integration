f4a8e9cc41ab9343471cab445855a337a6f2c615
saravila <s.vilarinol@gmail.com>
2013-12-22 23:58:03 +0100

    Agregado acceso directo en dashboard para creación de una partida clarcassone: accede al klarkihall y crea una partida el usuario registrado que pulsa el botón en cuestión sin pasar por usersinhall

--- PL-BlutBad/client/js/dashboard.js
+++ PL-BlutBad/client/js/dashboard.js
@@ -2,6 +2,34 @@ Template.dashboard.show = function() {
   return Session.get('current_stage') == 'Dashboard';
 };
 
+Template.dashboard.events({
+	'click .crea_party' : function() { 
+		Session.set('current_stage', 'klarkiHall');
+		if (Meteor.userId()) { 
+            userCreator = PartidasVolatiles.findOne({
+                creator_id : Meteor.userId()
+            })
+            if (userCreator) {
+                Session.set("createError", "Ya creaste una partida!");
+            } else { 
+                // ^.^ edad = Math.floor(Random.fraction() * 70);
+                PartidasVolatiles.insert({
+                    creator_id : Meteor.userId(),
+                    jugadores : [ {
+                        user_id : Meteor.userId(),
+                        estado : "Pendiente",
+                    } ],
+                    listos: false
+                }); 
+                Session.set("createError", undefined); 
+            }
+        } else {
+            Session.set("createError",
+                    "Regístrate para crear partidas!");
+        }
+	}
+});
+
 
 Template.misInsignias.juegos = function() {
 	  return Juegos.find();
