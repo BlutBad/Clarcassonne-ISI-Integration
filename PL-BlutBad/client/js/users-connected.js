7827b9224f62c77f1ae514c6cfb8719a09b259d1
vdjimenez <vd.jimenez89@gmail.com>
2013-12-28 12:38:31 +0100

    usersConnected v1.2

--- PL-BlutBad/client/js/users-connected.js
+++ PL-BlutBad/client/js/users-connected.js
@@ -1,4 +1,16 @@
-
+// Templates
 Template.connected.userConnected = function(){
 	return Meteor.users.find({});
 };
+
+
+// Events
+Template.usersConnected.events({
+	'click input#show-users-connected': function() {
+		$("#connected").show();
+	},
+
+	'click input#hide-users-connected': function() {
+		$("#connected").hide();
+	}
+});
