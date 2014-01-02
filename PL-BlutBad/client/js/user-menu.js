2e27c373196a4912f2a739f0bdd2f3e0dcc23202
itziar <itziar276@gmail.com>
2014-01-02 20:17:29 +0100

    se muestra el cuadro de perfil de usuario (cuadro parecido al de editar y crear torneos), solo falta que se rellene con los datos del user pedido y poner el boton de guardar como amigo para que cuando este hecho lo de los amigos se guarden

--- PL-BlutBad/client/js/user-menu.js
+++ PL-BlutBad/client/js/user-menu.js
@@ -11,26 +11,12 @@ Template.user_menu.current = function() {
   return Session.equals('current_stage', this.name) ? 'user-menu-current' : '';
 };
 
-Template.ffind.events({
-  'keydown input#tags': function(e){
-    if (e.which == 13) {
-      var name=$("#tags").val();
-      if (Meteor.users.findOne({username: name})){
-        console.log("esta");
-        console.log(Meteor.users.findOne({username: name})._id);
-        return Session.set('showprofile', this._id); 
-      }else{
-        console.log("no esta");
-      }
-    };
-  }
-});
 
 Template.user_menu.events({
   // Si esta pulsado el boton del raton sobre alguno de los menus
   // Si estas pinchando sobre el mismo tag que ya esta seleccionado
   'mousedown li' : function() {
-    //console.log(this.name);
+    console.log(this.name);
     // Si estas pinchando sobre el mismo tag que ya esta seleccionado
     if (Session.equals('current_stage', this.name)){
     // Poner lo a null, es decir celeccionar por defecto
@@ -38,6 +24,45 @@ Template.user_menu.events({
     }else{
     // De otra manera apuntar nuevo id del tag seleccionado
       Session.set('current_stage', this.name);}
+    },
+     'keydown input#tags': function(e){
+    if (e.which == 13) {
+      var name=$("#tags").val();
+      if (Meteor.users.findOne({username: name})){
+        console.log("esta");
+        console.log(Meteor.users.findOne({username: name})._id);
+         Session.set("profilfriend", Meteor.users.findOne({username: name})._id);
+       // return Session.set('current_stage', 'PerfilAmigo');
+  
+      }else{
+        console.log("no esta");
+      }
+    };
+  }
+
+});
+
+Template.friendprofil.events({
+
+   
+
+    'click .cancel' : function() {
+        Session.set('profilfriend', null); 
     }
 
 });
+
+Template.friendprofil.torneo = function() {
+    var id = Session.get('profilfriend');
+    //console.log(id)
+    return Meteor.users.findOne({
+        _id : id
+    });
+};
+
+Template.user_menu.showprofilfriend = function() {
+    return Session.get('profilfriend', this._id); 
+}   
+
+
+
