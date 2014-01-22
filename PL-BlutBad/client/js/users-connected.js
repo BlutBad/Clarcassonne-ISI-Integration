6ba1b6fde59cbbf50b74a4e9374c28663f5fa378
vdjimenez <vd.jimenez89@gmail.com>
2014-01-22 16:21:21 +0100

    probando ventana de amigos

--- PL-BlutBad/client/js/users-connected.js
+++ PL-BlutBad/client/js/users-connected.js
@@ -1,11 +1,18 @@
 //Templates
 Template.connected.userConnected = function() {
-	console.log("mi usurname is: ");
-	var myUsersId = Meteor.userId();
-	console.log(myUsersId);
-	var myUser = Meteor.users.findOne({_id: myUsersId});
+	// console.log("mi usurname is: ");
+	// var myUsersId = Meteor.userId();
+	// console.log(myUsersId);
+	// var myUser = Meteor.users.findOne({_id: myUsersId});
+	//return Friends.findOne({username: Meteor.user().username}).friends;
 	//console.log(myUser.username);
-	//return Friends.findOne({username: myFriends}).friends;
+	
+	//return Friends.findOne({username: myUser.username}).friends;
+	// var myUser = Meteor.users.findOne({_id: myUsersId});
+	// console.log(myUser);
+	// console.log(myUser.username);
+	// return Friends.findOne({username: myUser.username}).friends;
+	return Friends.findOne({username: Meteor.user().username}).friends;
 };
 
 
