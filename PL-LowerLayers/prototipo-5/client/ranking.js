bdb07341b984395d288dfac3ea7a7df4b105e751
hpmiguel <hpmiguel@hotmail.com>
2014-01-23 18:40:19 +0100

    Arreglada interfaz ranking

--- PL-LowerLayers/prototipo-5/client/ranking.js
+++ PL-LowerLayers/prototipo-5/client/ranking.js
@@ -3,65 +3,108 @@ Template.gamesrankingtemp.games=function(){
 	return Games.find();
 }
 
+
+///Eventos en plantillas////
+
 //Mostramos puntuaciones para un juego
 Template.gamesrankingtemp.events = {
 	'click .linkgameranking':function(event){
-		$("#ranking").children().hide();
-		$("#gameRanking").html($(this)[0].name);
-		Session.set('game_id', $(this)[0]._id);
+		$("#gamesranking").hide();
+		Session.set('game_id_ranking', $(this)[0]._id);
 		$("#bygameranking").fadeIn();
 	}
 }
 
-// Mostramos puntuaciones para un usuario
-Template.userstemp.events = {
-	'click .linkuserranking':function(event){
-		$("#ranking").children().hide();
-		$("#usernameRanking").html($(this)[0].username);
-		Session.set('user_id', $(this)[0]._id);
-		$("#byuserranking").fadeIn();
-	}
-}
+///Puntuaciones por juego, filtrar usuario////
 
-//Volvemos atras
-Template.byuserrankingtemp.events = {
-	'click a.linkback':function(event){
-		$("#ranking").children().fadeIn();
-		$("#byuserranking").hide();
-		$("#bygameranking").hide();
-		$("#usernameRanking").html("");
-	}
-}
-
-//Volvemos atras
 Template.bygamerankingtemp.events = {
+	//Volvemos atras
 	'click a.linkback':function(event){
-		$("#ranking").children().fadeIn();
-		$("#byuserranking").hide();
 		$("#bygameranking").hide();
-		$("#gameRanking").html("");
+		$("#gamesranking").fadeIn();
+	},
+	//filtramos por usuario con formulario
+	'click #btnuserranking':function(event){
+		var username = document.getElementById("formuserranking").value;
+		var user_id = Meteor.users.findOne({username:username})._id;
+		$("#bygameranking").hide();
+		Session.set('user_id_ranking', user_id);
+		$("#byusergameranking").fadeIn();
+	},
+	// filtramos por usuario con click en la tabla
+	'click a.linkuser':function(event){
+		$("#bygameranking").hide();
+		Session.set('user_id_ranking', $(this)[0].user_id);
+		$("#byusergameranking").fadeIn();
 	}
 }
 
-//Carga puntuaciones para usuario
-Template.byuserrankingtemp.ranking=function(){
-	if (Ranking.find().count()!=0){
-		var list = Ranking.find({user_id: Session.get("user_id")});
-		var list2=[];
-		list.forEach(function(elem) {
-			list2.push({"game":Games.findOne({_id:elem.game_id}).name,"user":elem.user_id, "score":elem.score});
-		});
-		return list2;
+////Puntuaciones de usuario en juego/////
+
+Template.byusergamerankingtemp.events = {
+	//Volvemos atras
+	'click a.linkback':function(event){
+		Session.set('user_id_ranking', undefined);
+		$("#byusergameranking").hide();
+		$("#bygameranking").fadeIn();
 	}
+}
+
+
+
+
+//////Helpers//////
+
+//Retorna nombre del juego elegido
+Template.bygamerankingtemp.gameranking=function(){
+	var game = undefined;
+	if (Session.get("game_id_ranking")) 
+		game = Games.findOne({"_id":Session.get("game_id_ranking")}).name;
+	return game;
+}
+
+//Comprueba que tiene que rellenar la plantilla por juego
+Template.bygamerankingtemp.user_not_selected=function(){
+	return Session.get("user_id_ranking")==undefined;
 }
 
 //Carga puntuaciones para juego
 Template.bygamerankingtemp.ranking=function(){
 	if (Ranking.find().count()!=0){
-		var list = Ranking.find({game_id: Session.get("game_id")});
+		var list = Ranking.find();
+		var list2=[];
+		Ranking.find().forEach(function(elem) {
+			list2.push({"user":Meteor.users.findOne({_id:elem.user_id}).username,
+					    "user_id":elem.user_id, "game_id":elem.game_id, "score":elem.score});
+		});
+		return list2;
+	}
+}
+
+
+//Retorna el nombre y el juego elegido
+Template.byusergamerankingtemp.gameranking=function(){
+	var gamerank = undefined;
+	if (Session.get("game_id_ranking")&&Session.get("user_id_ranking")){
+		var game=Games.findOne({"_id":Session.get("game_id_ranking")}).name;
+		var user=Meteor.users.findOne({"_id":Session.get("user_id_ranking")}).username;
+		gamerank = {user:user,name:game};
+	}
+	return gamerank;
+}
+
+//Comprueba que tiene que rellenar la plantilla por juego y usuario
+Template.byusergamerankingtemp.user_selected=function(){
+	return Session.get("user_id_ranking");
+}
+
+//Carga puntuaciones para juego y un usuario
+Template.byusergamerankingtemp.ranking2=function(){
+	if (Ranking.find().count()!=0){
+		var list = Ranking.find();
 		var list2=[];
 		list.forEach(function(elem) {
-			list2.push({"game":elem.game_id,"user":Meteor.users.findOne({_id:elem.user_id}).username, "score":elem.score});
+			list2.push({"score":elem.score});
 		});
 		return list2;
 	}
