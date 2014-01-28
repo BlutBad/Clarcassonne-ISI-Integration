23b919f8d9535fc90db9aa14c1f108b0f6b31ed3
itziar <itziar276@gmail.com>
2014-01-28 15:38:52 +0100

    terminos y condiciones y correo de contacto

--- PL-BlutBad/client/js/aiuto.js
+++ PL-BlutBad/client/js/aiuto.js
@@ -1,3 +1,23 @@
 Template.ayuda.show = function() {
   	return Session.get('current_stage') == 'Ayuda';
 };
+var openCreateTerm = function () {
+	Session.set("creatTError", null);
+	Session.set("showtermine", true);
+}; 
+
+Template.ayuda.events = {    
+   
+	'click .terms': function() {
+		console.log("here");
+		openCreateTerm();
+	}	 
+}; 
+
+
+
+
+Template.ayuda.showterms = function() {
+    return Session.get('showtermine'); 
+};
+
